3b3147e22f8d90416e1d3dba3b386fb208a39fa4
nayyarv <nayyarv@gmail.com>
2020-11-29 18:58:25 +1100

    Adding the tictactoe files

---
+++ metagamer/environments/tictactoe.py
@@ -0,0 +1,163 @@
+"""
+This file provides some simple tools to simulate and work with a tic tac toe board.
+The functions provided assume the board is represented by
+    - np array of shape 3,3
+    - player 1 is 1, player 2 is -1 and empty is 0
+
+
+"""
+import logging
+
+import numpy as np
+import gym
+from gym import spaces, error
+
+from typing import Iterable, Any, Tuple, Dict, List, Optional, Iterator
+
+logger = logging.getLogger(__name__)
+
+
+def turn_iterator(turn_order: Iterable[Any]) -> Iterator[Any]:
+    """Infinitely iterates through the order provided"""
+    while True:
+        for turn in turn_order:
+            yield turn
+
+
+def check_win(board: np.array) -> int:
+    """
+    Given a 3*3 numpy array whose elements are 0, 1, -1
+    representing empty, player1 and player 2,
+    return the winner or the
+
+    Args:
+        board (np.array): 3 x 3 board
+
+    Returns:
+        int: 0  -> no winner
+             1  -> player 1
+             -1 -> player 2
+
+    """
+    assert board.shape == (3, 3)
+    for axis in [0, 1]:
+        axis_sum = board.sum(axis=axis)
+        if 3 in axis_sum:
+            return 1
+        elif -3 in axis_sum:
+            return -1
+
+    diag = board.diagonal().sum()
+    diag_inv = np.fliplr(board).diagonal().sum()
+    for diag_sum in [diag, diag_inv]:
+        if diag_sum == 3:
+            return 1
+        elif diag_sum == -3:
+            return -1
+
+    # no winner yet
+    return 0
+
+
+def to_one_hot(board: np.array) -> np.array:
+    """
+    Convert the representation to one hot encoding
+    We assume a 3x3 board input
+
+    Args:
+        board (np.array): 3 x 3
+
+    Returns:
+        3 x 3 x 3:
+            board[:,: 0] = location of empty squares
+            board[:, :, 1] = location of player 1
+            board[:, :, 2] = location of player 2 moves
+
+    """
+    oh = np.stack((board == 0, board == 1, board == -1))
+    return oh.shape.astype(int)
+
+
+def to_human(board: np.array) -> np.array:
+    """Convert this into a """
+    human_board = np.full(board.shape, " ")
+    human_board[np.where(board == 1)] = "X"
+    human_board[np.where(board == -1)] = "O"
+    return human_board
+
+
+class TicTacToeEnv(gym.Env):
+    """
+    TicTacToe
+
+    """
+
+    metadata = {"render.modes": ["human"]}
+
+    TURN_ORDER = [1, -1]
+
+    def __init__(self):
+        self.board_shape = 3, 3
+        self.symbols = {1: "X", -1: "O"}
+        self.action_space = spaces.Tuple([spaces.Discrete(3), spaces.Discrete(3)])
+        # we don't support this right now
+        self.observation_space = None
+
+        self.board = None
+        self.turn_iterator = None
+        self.curr_turn = None
+        self.done = None
+
+        self.reset()
+
+    def reset(self):
+        self.board = np.zeros(self.board_shape, dtype=int)
+        self.turn_iterator = turn_iterator(self.TURN_ORDER)
+        self.curr_turn = next(self.turn_iterator)
+        self.done = False
+
+    def _get_obs(self):
+        return self.board
+
+    def step(
+        self, action: Tuple[int, int], player: Optional[int] = None
+    ) -> Tuple[Any, float, bool, Dict]:
+        action = tuple(action)
+        if self.board[action] != 0:
+            raise error.InvalidAction(f"action {action} is not a vaid choice")
+        if self.done:
+            raise error.ResetNeeded("Call reset as game is over")
+
+        logger.debug("Selected action: %s", action)
+
+        self.board[action] = self.curr_turn
+
+        reward = check_win(self.board)
+        if reward:
+            self.done = True
+            return self._get_obs(), float(reward), self.done, {}
+
+        if self.turns_played == 9:
+            # draw
+            self.done = True
+            return self._get_obs(), 0.0, self.done, {}
+
+        self.curr_turn = next(self.turn_iterator)
+        return self._get_obs(), 0.0, self.done, {}
+
+    @property
+    def valid_actions(self) -> List[Tuple[int, int]]:
+        return [tuple(act) for act in np.argwhere(self.board == 0).tolist()]
+
+    @property
+    def turns_played(self):
+        return np.sum(self.board != 0)
+
+    def render(self, mode="human"):
+        import tabulate
+        tabulate.PRESERVE_WHITESPACE = True
+
+        human_board = to_human(self.board)
+        print("\n")
+        print(f"Turn : {self.turns_played}")
+        print(tabulate.tabulate(human_board.tolist(), tablefmt="fancy_grid"))
---
+++ tests/test_tictactoe.py
@@ -0,0 +1,90 @@
+import pytest
+import numpy as np
+import random
+from metagamer.environments import tictactoe
+
+
+@pytest.fixture
+def tt():
+    return tictactoe.TicTacToeEnv()
+
+
+def test_simple_game(tt):
+    _, reward, done, _ = tt.step((0, 0))
+    assert not done
+    assert reward == 0
+    _, reward, done, _ = tt.step((0, 1))
+    assert not done
+    assert reward == 0
+    _, reward, done, _ = tt.step((1, 0))
+    assert not done
+    assert reward == 0
+    _, reward, done, _ = tt.step((1, 1))
+    assert not done
+    assert reward == 0
+
+    _, reward, done, _ = tt.step((2, 0))
+    assert done
+    assert reward == 1
+
+
+@pytest.mark.parametrize("player", (1, -1))
+def test_diagonal(player):
+    board = np.zeros((3, 3))
+    board[0, 0] = player
+    board[1, 1] = player
+    board[2, 2] = player
+    assert tictactoe.check_win(board) == player
+
+
+@pytest.mark.parametrize("player", (1, -1))
+def test_rev_diagonal(player):
+    board = np.zeros((3, 3))
+    board[0, 2] = player
+    board[1, 1] = player
+    board[2, 0] = player
+    assert tictactoe.check_win(board) == player
+
+
+@pytest.mark.parametrize("seed", range(100))
+def test_random(tt, seed):
+    random.seed(seed)
+    while not tt.done:
+        action = random.choice(tt.valid_actions)
+
+        _, reward, *_ = tt.step(action)
+        assert tt.board.sum() in {0, 1}
+    assert tt.turns_played >= 5
+    assert reward in {-1, 0, 1}
+
+
+def test_render(tt, capsys):
+    tt.step((0, 0))
+    tt.render()
+    captured = capsys.readouterr()
+    assert captured.out == """
+
+Turn : 1
+╒═══╤═══╤═══╕
+│ X │   │   │
+├───┼───┼───┤
+│   │   │   │
+├───┼───┼───┤
+│   │   │   │
+╘═══╧═══╧═══╛
+"""
+
+    tt.step((2, 1))
+    tt.render()
+    captured = capsys.readouterr()
+    assert captured.out == """
+
+Turn : 2
+╒═══╤═══╤═══╕
+│ X │   │   │
+├───┼───┼───┤
+│   │   │   │
+├───┼───┼───┤
+│   │ O │   │
+╘═══╧═══╧═══╛
+"""
